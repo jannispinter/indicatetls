0fb0483c8afc8d8e420815b9be6a0483db2a1951
Jannis Pinter <jannis@pinterjann.is>
2020-01-25 19:10:58 +0100

    Do not show a warning triangle on the page action icon, fixes #18

--- background.js
+++ background.js
@@ -6,14 +6,6 @@ versionIconMap.set('TLSv1', 'icons/tlsv10.png');
 versionIconMap.set('SSLv3', 'icons/sslv3.png'); /* no longer supported */
 versionIconMap.set('unknown', 'icons/tlsunknown.png');
 
-var versionIconWarningMap = new Map();
-versionIconWarningMap.set('TLSv1.3', 'icons/tlsv13_warning.png'); 
-versionIconWarningMap.set('TLSv1.2', 'icons/tlsv12_warning.png'); 
-versionIconWarningMap.set('TLSv1.1', 'icons/tlsv11_warning.png'); 
-versionIconWarningMap.set('TLSv1', 'icons/tlsv10_warning.png'); 
-versionIconWarningMap.set('SSLv3', 'icons/sslv3_warning.png'); /* no longer supported */
-versionIconWarningMap.set('unknown', 'icons/tlsunknown.png');
-
 var versionComparisonMap = new Map();
 versionComparisonMap.set('TLSv1.3', 13); 
 versionComparisonMap.set('TLSv1.2', 12); 
@@ -25,16 +17,10 @@ versionComparisonMap.set('unknown', 0);
 var tabMainProtocolMap = new Map();
 var tabSubresourceProtocolMap = new Map();
 
-async function updateIcon(tabId, protocolVersion, warning) {
-    if (warning) {
-        browser.pageAction.setIcon({
-            tabId: tabId, path: versionIconWarningMap.get(protocolVersion)
-        });
-    } else {
-        browser.pageAction.setIcon({
-            tabId: tabId, path: versionIconMap.get(protocolVersion)
-        }); 
-    }
+async function updateIcon(tabId, protocolVersion) {
+    browser.pageAction.setIcon({
+        tabId: tabId, path: versionIconMap.get(protocolVersion)
+    }); 
     browser.pageAction.setTitle({tabId: tabId, title: protocolVersion});
     browser.pageAction.setPopup({tabId: tabId, popup: "/popup/popup.html"});
 }
@@ -62,11 +48,11 @@ async function processSecurityInfo(details) {
         /* set the icon correctly */
         if (details.type === 'main_frame') {
             tabMainProtocolMap.set(details.tabId, securityInfo.protocolVersion);
-            await updateIcon(details.tabId, securityInfo.protocolVersion, false);
+            await updateIcon(details.tabId, securityInfo.protocolVersion);
         } else {
             cached_version = tabMainProtocolMap.get(details.tabId);
             if (typeof cached_version !== "undefined") {
-                await updateIcon(details.tabId, cached_version, false);
+                await updateIcon(details.tabId, cached_version);
             }
         }
 
@@ -75,13 +61,13 @@ async function processSecurityInfo(details) {
         subresourceMap.set(host, securityInfo);
         tabSubresourceProtocolMap.set(details.tabId, subresourceMap);
 
-        var mainProtocolVersion = versionComparisonMap.get(tabMainProtocolMap.get(details.tabId));
+        /*var mainProtocolVersion = versionComparisonMap.get(tabMainProtocolMap.get(details.tabId));
         for (const securityInfo of subresourceMap.values()) {
             if (versionComparisonMap.get(securityInfo.protocolVersion) < mainProtocolVersion) {
                 await updateIcon(details.tabId, tabMainProtocolMap.get(details.tabId), true);
                 break;
             }
-        }
+        }*/
 
     } catch(error) {
         console.error(error);
